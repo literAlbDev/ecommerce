dd696940ad44b5095b37d9d3548c80fb6994d0c1
liter-alb-dev <liter.alb.dev@gmail.com>
2023-09-22 10:15:31 +0300

    themed the app, added routes

---
+++ lib/common/AppColorScheme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColorScheme{
+  static Color primary = Colors.orange;
+  static Color onPrimary = Colors.white;
+  static Color secondary = Colors.blue;
+  static Color onSecondary = Colors.white;
+  static Color background = Colors.white;
+  static Color surface = Colors.orange[100]!;
+  static Color onSurface = Colors.black;
+  static Color hint = Colors.grey;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/common/AppColorScheme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,27 +12,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ecommerce',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme(
+            brightness: Brightness.light,
+            primary: AppColorScheme.primary,
+            onPrimary: AppColorScheme.onPrimary,
+            secondary: AppColorScheme.secondary,
+            onSecondary: AppColorScheme.onSecondary,
+            error: Colors.red,
+            onError: Colors.white,
+            background: AppColorScheme.background,
+            // Change
+            onBackground: Colors.grey,
+            surface: AppColorScheme.surface,
+            onSurface: AppColorScheme.onSurface),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          //elevation: 0,
+          surfaceTintColor: Colors.white
+        ),
+        hintColor: AppColorScheme.hint,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          filled: true,
+          hintStyle: const TextStyle(fontSize: 13),
+          suffixIconColor: AppColorScheme.hint,
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            textStyle: TextStyle(
+              fontWeight: FontWeight.w400,
+            ),
+            minimumSize: Size(double.infinity, 55),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        //'/signin': (context) => SigninPage(),
+        //'/signup': (context) => SignupPage(),
+        //'/profile': (context) => ProfilePage(),
+        //'/home': (context) =>  HomePage(),
+        //'/product': (context) =>  ProductPage(),
+        //'/whishlist': (context) =>  WhishlistPage(),
+        //'/orders': (context) =>  OrdersPage(),
+      },
+      //home: const SigninPage(),
     );
   }
 }
